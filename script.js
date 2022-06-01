13da4b0ba594dd580a146ad68b93a98419d90690
Josh Adams <joshuaadams@Joshs-MacBook-Pro.local>
2022-06-01 17:56:53 -0600

    upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+game = {};
+
+game.killer = '?'
+
+game['weapons'] = [
+    {type: 'lasers', location: 'lab'},
+    {type: 'angry cats', location: 'basement'},
+    {type: 'handgrenade', location: 'back yard'}
+];
+
+game.name = [];
+game.name[0] = 'Miss Scarlet';
+game.name.push('Mr. Green');
+
+console.log(game);
